f254626491eae7488dd44698de145d578109a6e2
Omar <omarfkuri@gmail.com>
2024-01-26 21:58:06 -0600

    Deprecate Component

--- src/api/Component.ts
+++ src/api/Component.ts
@@ -2,18 +2,29 @@ import type { Comp } from "../class/Base.js"
 
 
 
+/**@deprecated*/
 export function Component<T extends {[key: string]: any} = {}>(comp: Component<T>) 
 {
 	return comp;
+}
+
+export function eComp<T extends {[key: string]: any} = {}>(comp: eComp<T>)
+{
+  return comp;
 }
 
 
 
 declare global 
 {
-
+  /**@deprecated*/
 	type Component<T extends {[key: string]: any} = {}> = 
   {
 		(this: Comp<T>, props: T, comp: Comp<T>): JSX.Element
 	}
+  
+  type eComp<T extends {[key: string]: any} = {}> = 
+  {
+    (this: Comp<T>, props: T, comp: Comp<T>): JSX.Element
+  }
 }
